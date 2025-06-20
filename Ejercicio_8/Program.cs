886837aa553c21c559fe5795030bd676627917e6
Gemino <71484444@idexperujapon.edu.pe>
2025-06-20 17:02:50 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+            Console.WriteLine("Ingrese su nota:");
+            nota=Convert.ToInt32(Console.ReadLine());
+            if (nota >= 16)
+            {
+                Console.WriteLine("Excelente");
+            }
+            else if (nota == 14 || nota == 15)
+            {
+                Console.WriteLine("Muy bueno");
+            }
+            else if (nota <= 13 && nota >= 11)
+            {
+                Console.WriteLine("Bueno");
+            }
+            else
+            {
+                Console.WriteLine("Insuficiente");
+            }
+        }
+    }
+}
