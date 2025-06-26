d89a5144aecf8ccfd514a7149789bfd069d12800
Gemino <71484444@idexperujapon.edu.pe>
2025-06-26 17:29:52 -0500

    finalizado

--- Ejercicio_8/Program.cs
+++ Ejercicio_8/Program.cs
@@ -10,19 +10,23 @@ namespace Ejercicio_8
     {
         static void Main(string[] args)
         {
+            //declaramos variables y pedimos datos
             int nota;
             Console.WriteLine("Ingrese su nota:");
             nota=Convert.ToInt32(Console.ReadLine());
+            //while se ejecuta continuamente hasta que ingrese el valor solicitado
             while (nota < 0 || nota > 20)
             {
                 Console.WriteLine("El valor es incorrecto");
                 Console.WriteLine("Ingrese una nota entera entre 0 y 20:");
                 nota= Convert.ToInt32(Console.ReadLine());
             }
+            //es el quivalente al si
             if (nota >= 16)
             {
                 Console.WriteLine("Excelente");
             }
+            //else if se usa cuando vas a ingresar varias "sino"
             else if (nota == 14 || nota == 15)
             {
                 Console.WriteLine("Muy bueno");
@@ -31,6 +35,7 @@ namespace Ejercicio_8
             {
                 Console.WriteLine("Bueno");
             }
+            //el último "sino" debe ser solo else
             else
             {
                 Console.WriteLine("Insuficiente");
